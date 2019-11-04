3a0a4b1f5ef34afbf5cf23cdf7494a8e8e8fac08
CF <1578781015@qq.com>
2019-11-04 16:03:59 +0800

    添加跨平台设置变量插件

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import { globalContext } from "@src/redux/reducer";
 import style from './index.scss'
 
-export default function Header(){
+export default function Header() {
     const { state } = useContext(globalContext);
     return (
-        <div className={ style.header }>
-            这是头部, 用户名为{ state.userInfo && state.userInfo.name }
+        <div className={style.header}>
+            这是头部, 用户名为{state.userInfo && state.userInfo.name}
             <Link to="/router1">路由1</Link>
             <Link to="/router2">路由2</Link>
         </div>
